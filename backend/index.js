f6e4195834f64d4a3a535e93a22d43cddfa20dc2
surendersk1065 <surendersk1065@gmail.com>
2026-07-13 20:45:07 +0530

    o

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,33 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
+const dns = require("dns")
+dns.setServers(["1.1.1.1","8.8.8.8"])
+const uri = "mongodb+srv://sk:2002@cluster0.en56xwo.mongodb.net/passkey?appName=Cluster0"
 
 
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: "https://bulkmailapp-nine.vercel.app",
-    methods: ["GET", "POST"],
-    credentials: true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://bulkmailapp-nine.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
 app.use(express.json())
 app.listen(3000, function () {
     console.log("server started")
 })
 
-mongoose.connect("mongodb://user1:2002@ac-8yztyxf-shard-00-00.en56xwo.mongodb.net:27017,ac-8yztyxf-shard-00-01.en56xwo.mongodb.net:27017,ac-8yztyxf-shard-00-02.en56xwo.mongodb.net:27017/passkey?ssl=true&replicaSet=atlas-139ih2-shard-0&authSource=admin&appName=Cluster0").then(function () {
+mongoose.connect(uri).then(function () {
     console.log("connected")
 }).catch(function (err) {
     console.log(err)
 })
-const details = mongoose.model("details", {}, "bulkmail")
+const bulkmail = mongoose.model("bulkmail", {}, "bulkmail")
 
 
 const nodemailer = require("nodemailer")
@@ -30,7 +36,7 @@ app.post("/sendmail", function (req, res) {
     var msg = req.body.msg
     var email = req.body.email
 
-    details.find().then(function (data) {
+    bulkmail.find().then(function (data) {
         const transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
@@ -38,29 +44,28 @@ app.post("/sendmail", function (req, res) {
                 pass: data[0].toJSON().pass
             }
         })
-new Promise(async function (resolve, reject) {
-    try {
-        for (let i = 0; i < email.length; i++) {
-            await transporter.sendMail({
-                from: data[0].toJSON().user,
-                to: email[i],
-                subject: "A message from node mailer",
-                text: msg
-            });
-        }
-        resolve("success");
-    } catch (error) {
-        console.log(error);   // Print the real error
-        reject(error);
-    }
-})
-.then(() => {
-    res.send(true);
-})
-.catch((error) => {
-    console.log(error);
-    res.send(false);
-});
+        new Promise(async function (resolve, reject) {
+            try {
+                for (i = 0; i < email.length; i++) {
+                    await transporter.sendMail(
+                        {
+                            from: "surendersk1065@gmail.com",
+                            to: email[i],
+                            subject: "A message from node mailer",
+                            text: msg
+                        },
+                    )
+                }
+                resolve("success")
+            }
+            catch (error) {
+                reject("failed")
+            }
+        }).then(function () {
+            res.send(true)
+        }).catch(function () {
+            res.send(false)
+        })
     }).catch(function (error) {
         console.log(error)
     })
